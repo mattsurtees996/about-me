336b17721ffe0338f0707c2fbe69eaa0c7c0da59
mattsurtees996 <matthew.surtees@icloud.com>
2023-01-23 14:56:08 +0000

    update website with questions

---
+++ app.js
@@ -0,0 +1,37 @@
+let user = prompt("What is your name?");
+console.log("The user's name is " + user);
+
+let question1 = confirm("was i born in 1996?");
+if (question1 == true) {
+  console.log("correct!");
+} else {
+  console.log("incorrect!");
+}
+
+let question2 = confirm("Did I play ice hockey?");
+if (question2 == true) {
+  console.log("correct!");
+} else {
+  console.log("incorrect!");
+}
+
+let question3 = confirm("Do I have a 6 year old daughter?");
+if (question3 == true) {
+  console.log("correct!");
+} else {
+  console.log("incorrect!");
+}
+
+let question4 = confirm("Is my favourite cuisine Thai?");
+if (question4 == true) {
+  console.log("correct!");
+} else {
+  console.log("incorrect!");
+}
+
+let question5 = confirm("Do I support Tottenham Hotspurs?");
+if (question5 == true) {
+  console.log("correct!");
+} else {
+  console.log("incorrect!");
+}
